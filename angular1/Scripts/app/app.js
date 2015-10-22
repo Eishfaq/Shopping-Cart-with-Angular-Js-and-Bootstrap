d9e82165c2f03d1a595b69cc5b4fe3e13f8ccfba
Eishfaq Ahmed Shafa <eaeishfaq1991@outlook.com>
2015-10-23 02:03:47 +0600

    Starting ... Choosing products and they are adding in a cart with total price, user can delete the choose products

---
+++ angular1/Scripts/app/app.js
@@ -0,0 +1,11 @@
+﻿
+
+var app = angular.module('myApp', ['ui.router','ngResource']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/brand");
+
+    $stateProvider.state('view1', { url: '/view1', templateUrl: 'Partials/home.html', controller: 'myController1' });
+    $stateProvider.state('view2', { url: '/view2', templateUrl: 'Partials/cart.html', controller: 'myController1' });
+    $stateProvider.state('brand', { url: '/brand', templateUrl: 'Partials/welcome.html' });
+});
---
+++ angular1/Scripts/app/controller.js
@@ -0,0 +1,45 @@
+﻿
+app.controller('myController1', ['$scope', function($scope) {
+    $scope.clocks = [
+        { name: 'Fast Track', price: 1200, image: '/images/fasttrack.jpg'},
+        { name: 'Casio', price: 1400, image: '/images/casio.jpg' },
+        { name: 'Omax', price: 1300, image: '/images/omax.jpg' },
+        { name: 'Titan', price: 1800, image: '/images/titan.jpg' },
+        { name: 'Rado', price: 2500, image: '/images/rado.jpg' },
+        { name: 'Current', price: 3000, image: '/images/titan.jpg' },
+        { name: 'Tissot', price: 2500, image: '/images/tissot.jpg' },
+        { name: 'Rolex', price: 3000, image: '/images/rolex.jpg' },
+        { name: 'Roland', price: 1800, image: '/images/roland.jpg' }
+       
+        
+    ];
+
+   
+
+    $scope.list = [];
+
+    $scope.ButtonName = "Add To Cart";
+
+    $scope.addItem = function(product) {
+
+       
+        $scope.list.push(product);
+        
+    };
+
+
+    $scope.getTotal = function() {
+        var total = 0;
+        for (var i = 0; i < $scope.list.length; i++) {
+            var product = $scope.list[i];
+            total += product.price;
+        }
+        return total;
+    };
+
+    $scope.delete = function(rv) {
+        var index = $scope.list.indexOf(rv);
+        $scope.list.splice(index, 1); 
+    };
+}]);
+
